9fc0bef5072b015edefebba5d1895ccf2d332190
PureWasian <alancooper509@hotmail.com>
2020-12-27 03:02:20 -0600

    realized discord embeds for item display were prone to errors due to 1024 char max in embed fields, swapped to manual pagination in a message outside of the embed

--- commands/shop_browse.js
+++ commands/shop_browse.js
@@ -30,8 +30,7 @@ module.exports = (message) => {
 			else resolve(rows);
 		});
 	});
-	// TODO: purchased query
-	
+
 	// execute queries
 	Promise.all([shopQuery, userQuery, soldQuery]).then( results => {
 		db.close();
@@ -44,80 +43,114 @@ module.exports = (message) => {
 			itemLists = {};
 			shopRows.forEach(row => {
 				let size = row.size === 'S' ? emotes.small : emotes.large;
-				let name = inventory.filter(entry => entry.item_name == row.name).length > 0 ? `~~\`${row.name}\`~~` : `\`${row.name}\``;
-				let cost = row.price_type === 'F' ? `${emotes.fish} ${row.price_amount}` : `${emotes.goldfish} ${row.price_amount}`;
-				let rowEntry = `${size} ${name} ${cost}`; // to be formatted after compiling
-				if (Object.keys(itemLists).includes(row.category)) {
-					itemLists[row.category].push(rowEntry);
+				let name = inventory.filter(entry => entry.item_name == row.name).length > 0 ? `~~\`${rightPadding(row.name)}\`~~` : `\`${rightPadding(row.name)}\``;
+				let cost = row.price_type === 'F' ? `\`${leftPadding(row.price_amount)}\` ${emotes.fish}` : `\`${leftPadding(row.price_amount)}\` ${emotes.goldfish}`;
+				let rowEntry = `${size} │ ${name} │ ${cost}`; // to be further formatted after compiling
+
+				// pagination of categories
+				let pageNumber = 1;
+				let entriesPerPage = 6;
+				while( `${row.category} (Page ${pageNumber})` in itemLists && itemLists[`${row.category} (Page ${pageNumber})`].length > entriesPerPage) {
+					pageNumber++;
+				};
+
+				// create or add row entry to appropriate category and page
+				if (`${row.category} (Page ${pageNumber})` in itemLists) {
+					itemLists[`${row.category} (Page ${pageNumber})`].push(rowEntry);
 				} else {
-					itemLists[row.category] = [rowEntry];
+					itemLists[`${row.category} (Page ${pageNumber})`] = [rowEntry];
 				}
 			});
 
-			// right padding with spaces to match item name widths in each category
-			Object.keys(itemLists).forEach(key => {
-				itemLists[key] = addSpaces(itemLists[key]);
-			});
-
-			// create embed specifying category to display
-			const shopEmbed = createEmbed(userRow, itemLists, current_category = "Balls");
-
-			// send embed
-			let embed; // for scope
-			message.channel.send(shopEmbed).then(embedMessage => {
-				embed = embedMessage;
-				return embed.react(emoteID(emotes.left));
-			}).then(() => {
-				return embed.react(emoteID(emotes.right));
-			}).then(() => {
-				const filter = (reaction, user) => {
-					return [emoteID(emotes.left), emoteID(emotes.right)].includes(reaction.emoji.id) && user.id === message.author.id;
-				};	
-				const collector = embed.createReactionCollector(filter, { dispose: true, time: 60000 });
-				collector.on("collect", (reaction) => {
-					current_category = updateEmbed(embed, reaction, itemLists, current_category, userRow);
-				});
-				collector.on("remove", (reaction) => {
-					current_category = updateEmbed(embed, reaction, itemLists, current_category, userRow);
-				});
-				collector.on("end", () => {
-					embed.react('🚫');
-				});
-			});
+			sendReplies(message, userRow, itemLists);
 		}
 	});
 }
 
-function addSpaces(stringArray) {
-	let maxLength = stringArray.reduce((a, b) => { return a.split('`')[1].length > b.split('`')[1].length ? a : b; }).split('`')[1].length;
-	stringArray.forEach((val, idx) => {
-		let tokens = val.split('`');
-		let spaces = '';
-		for (let i = tokens[1].length; i < maxLength; i++) { spaces += ' '; }
-		stringArray[idx] = `${tokens[0]}\`${tokens[1]}${spaces}\`${tokens[2]}`;
-	});
-	return stringArray;
-}
-
-function createEmbed(userRow, itemLists, current_category) {
-	return new Discord.MessageEmbed()
+function sendReplies(message, userRow, itemLists) {
+	// create embed specifying shop information
+	const shopEmbed = new Discord.MessageEmbed()
 		.attachFiles(["images/logos/Button_shop.png", "images/logos/atsume.jpg"])
 		.setAuthor("Goodies Shop", "attachment://atsume.jpg")
 		.setThumbnail("attachment://Button_shop.png")
-		.setDescription("> Purchase goodies with\n> **%shop [goodie-name]**")
-		.addField('\u200B', `**Current Balance**\n${emotes.fish} ${userRow.fish_count} ${emotes.goldfish} ${userRow.goldfish_count}`)
-		.addField(`Category: ${current_category}`, itemLists[current_category].join('\n') + "\n\u200B")
-		.setFooter("Browse using the arrows below (1 minute)");
+		.setDescription("> Purchase new goodies using:\n> **%shop [goodie-name]**")
+		.addField("Current Balance:", `${userRow.fish_count} ${emotes.fish}`, true)
+		.addField('\u200b', `${userRow.goldfish_count} ${emotes.goldfish}`, true);
+
+	let current_category = "Balls (Page 1)";
+	message.channel.send(shopEmbed).then(() => {
+		// display first shop page
+		return message.channel.send(createTable(current_category, itemLists));
+	}).then(tableMessage => {
+		// display emotes to flip shop pages
+		return tableMessage.react(emoteID(emotes.left));
+	}).then((tableMessageReact) => {
+		return tableMessageReact.message.react(emoteID(emotes.right));
+	}).then((tableMessageReact) => {
+		let tableMessage = tableMessageReact.message;
+		// define listener for when original author emotes to flip shop pages
+		const filter = (reaction, user) => {
+			return [emoteID(emotes.left), emoteID(emotes.right)].includes(reaction.emoji.id) && user.id === message.author.id;
+		};	
+		const collector = tableMessage.createReactionCollector(filter, { dispose: true, time: 60000 });
+		collector.on("collect", (reaction) => {
+			current_category = updateTable(tableMessage, reaction, current_category, itemLists);
+		});
+		collector.on("remove", (reaction) => {
+			current_category = updateTable(tableMessage, reaction, current_category, itemLists);
+		});
+		collector.on("end", () => {
+			tableMessage.react('🚫');
+		});
+	});
 }
 
-function updateEmbed(embed, reaction, itemLists, current_category, userRow) {
+function rightPadding(inputString) {
+	// Goal: right padding with spaces to match item name widths
+	let maxLength = 22; // "Grass Cushion (Purple)" from analyzing the GoodiesShop table in memory.db
+	let spaces = '';
+	for (let i = inputString.length; i < maxLength; i++) { spaces += ' '; }
+	return `${inputString}${spaces}`;
+}
+
+function leftPadding(inputString) {
+	// Goal: left padding with spaces to match price_amount widths
+	let maxLength = 4; // Antique Chair (1500 fish)
+	let zeros = '';
+	for (let i = inputString.toString().length; i < maxLength; i++) { zeros += '0'; }
+	return `${zeros}${inputString}`;
+}
+
+function createTable(current_category, itemLists) {
+	let width = 41;
+	let title = `Category: ${current_category}`;
+
+	let spaces = '';
+	for (let i = title.length; i < width; i++) { spaces += ' '; }
+	let header = `\`${title}${spaces}\``
+
+	let topBorder = '';
+	for (let i = 0; i < width; i++) {topBorder += '_'; }
+	topBorder = `\`${topBorder}\``;
+	
+	let border = "`│`";
+	let rows = `${border}${itemLists[current_category].join(`${border}\n${border}`)}${border}`;
+	
+	let bottomBorder = '';
+	for (let i = 0; i < width; i++) {bottomBorder += '¯'; }
+	bottomBorder = `\`${bottomBorder}\``;
+
+	return `${header}\n${topBorder}\n${rows}\n${bottomBorder}`;
+}
+
+function updateTable(table, reaction, current_category, itemLists) {
 	let offset = 0;
 	if (reaction.emoji.name == "left") offset = -1;
 	else if (reaction.emoji.name == "right") offset = 1;
 	else return current_category;
 	let keys = Object.keys(itemLists);
 	let new_category = keys[(keys.indexOf(current_category) + offset + keys.length) % keys.length];
-	let new_embed = createEmbed(userRow, itemLists, new_category);
-	embed.edit(new_embed);
+	let new_table = createTable(new_category, itemLists);
+	table.edit(new_table);
 	return new_category;
 }
